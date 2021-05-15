e9b3255d95feef578d128e682374ee6fc0160f3e
Jordan2112 <alejandroaleman93@outlook.com>
2021-05-15 13:56:25 -0500

    Cambio

--- Parcial 2/Login/js/Login.js
+++ Parcial 2/Login/js/Login.js
@@ -1,55 +1,14 @@
+function validar(){
+    var usuario = $("#user").val();
+    var password = $("#password").val();
+    
+    if(usuario == "Jordan" && password == "1234"){
+        
+        swal("Validación", "Bienvenido", "success");
+    }
+    else{
+        swal("Validación", "Usuario y/o contraseña invalidos", "error");
+    }
+ }
 
-
-    $('document').ready(function () {
-
-        $('#Boton').click(botonIniciarSesion);
-        
-        $('#Contraseña').keypress(function(e) {
-                if (e.keyCode == 13 ) { botonIniciarSesion(); }
-        });
-
-        function botonIniciarSesion() {
-
-            var vLog = $('#Login').val();
-            var vPas = $('#Contraseña').val();
-        
-                if (vLog == 'Jordan' && vPas=='Aleman') {
-        
-                    console.log('Inicio de sesion con exito');
-        
-                    $('#myModal .modal-header').addClass('modal-header-success');
-                    $('#myModal .modal-header h2').text('Acceso Concedido');
-                    $('#myModal .modal-body h3').text('Bienvenido');
-                    $('#myModal').modal();
-        
-                    $("#myModal").on('shown.bs.modal',function() {
-                        $('#botonCerrar').focus();
-                        });
-                    $("#myModal").on('hidden.bs.modal',function() {
-                    
-                        $('#myModal .modal-header').removeClass('modal-header-success');
-                        
-                        });
-        
-                    
-        
-                }
-                else {
-                    console.log('login incorrecto');
-        
-                    $('#myModal .modal-header').addClass('modal-header-danger');
-                    $('#myModal .modal-header h2').text('Error en datos');
-                    $('#myModal .modal-body h3').text('Intentelo de nuevo');
-                    $('#myModal').modal();
-        
-                    $("#myModal").on('shown.bs.modal',function() {
-                        $('#botonCerrar').focus();
-                    });
-                    $("#myModal").on('hidden.bs.modal',function() {
-                        $('#myModal .modal-header').removeClass('modal-header-danger');
-                        $('Boton').focus();
-                    });
-        
-                }
-        
-        }});
+    
