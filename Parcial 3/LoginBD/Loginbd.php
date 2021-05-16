3277c2594d988825fdc7b24bf86ac3463cd4f13b
Jordan2112 <alejandroaleman93@outlook.com>
2021-05-16 15:01:32 -0500

    LoginBD

---
+++ Parcial 3/LoginBD/Loginbd.php
@@ -0,0 +1,40 @@
+<?php
+
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "BD";
+
+
+    $conn = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
+
+
+    if(!$conn)
+    {
+
+        die("No hay concexion: " .mysqli_connect_error());
+
+    }
+
+    $Nombre = $_POST["Login"];
+    $Pass = $_POST["Contraseña"];
+
+    $query = mysqli_query($conn, "select * from usuario where Nombre =  '".$Nombre."'  and Contraseña = '".$Pass."'");
+    $nr = mysqli_num_rows($query);
+
+    if($nr == 1)
+    {
+
+        echo "Bievenido" .$Nombre;
+
+
+
+    }
+    else if($nr == 0)
+    {
+
+        echo "No ha ingresado";
+
+    }
+
+?>
