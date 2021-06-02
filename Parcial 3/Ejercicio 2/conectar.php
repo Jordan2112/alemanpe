c7674ab6c6935ba6e7c3f9ae74b8014f3f0a9ef2
Jordan2112 <alejandroaleman93@outlook.com>
2021-06-02 08:34:50 -0500

    a

--- Parcial 3/Ejercicio 2/conectar.php
+++ Parcial 3/Ejercicio 2/conectar.php
@@ -8,7 +8,7 @@ $password='';
 try {
       $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
 } catch(PDOException $e) {
-      echo "Error de conexion a la base de datos";
+      echo "Falla de conexion a la base de datos";
       echo $e->getMessage();
       exit();
 }
